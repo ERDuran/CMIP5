247d4ce13347c6732eeef15d3a0e4dcd03370a47
Earl Duran <earl.r.duran@gmail.com>
2018-04-12 13:04:45 +1000

    commit just before modifying p02 to account for 3D inputs
    --

--- Yin.sh
+++ Yin.sh
@@ -1,2 +1,1 @@
 scp *.py erd561@raijin.nci.org.au:~/CMIP5/.
-scp *.txt erd561@raijin.nci.org.au:~/CMIP5/.
--- p01_CMIP5_directories_maker.py
+++ p01_CMIP5_directories_maker.py
@@ -1,29 +1,40 @@
 import os
+import pickle
 
 CMIP5_dir = '/g/data1/ua6/DRSv2/CMIP5'
 
 erd561_dir = '/g/data/e14/erd561/CMIP5'
 
-output_dir = '/bromeliad'
+output_dir = raw_input('output directory? e.g. anthurium, bromeliad: ')
 
-my_dir = erd561_dir + output_dir
+my_dir = erd561_dir + '/' + output_dir
 
-models_list = sorted(os.listdir(CMIP5_dir))
+# models_list = sorted(os.listdir(CMIP5_dir))
 
-experiment = 'historical'
+# if var in ['tos', 'tauuo']:
+#     models_list = sorted(os.listdir(CMIP5_dir))
+# elif var in ['uas', 'vas']:
+#     with open('p05_index_and_pools.pkl', 'rb') as f:
+#         lat_warmN, lat_warmS, lon_warmW, lon_warmE, \
+#         SAM_index_sorted_cleaned, warm_pool_sorted_cleaned, wind_pool_sorted_cleaned = \
+#         pickle.load(f)
+#         models_list = sorted(SAM_index_sorted_cleaned)
+
+# print(models_list)
+
+experiment = raw_input('experiment? e.g. rcp85, historical: ')
 
 frequency = 'mon'
 
-medium = 'ocean'
+medium = raw_input('medium? e.g. ocean, atmos: ')
 
 initial_cond = 'r1i1p1'
 
-var = 'tos'
+var = raw_input('var? e.g. thetao, tauuo, uas, vas: ')
 
 version = 'latest'
 
-my_path = my_dir + '/' + experiment + '/' + frequency + '/' + \
-medium + '/' + initial_cond  + '/' +  var  + '/' +  version
+my_path = my_dir + '/' + experiment + '/' + frequency + '/' + medium + '/' +  var  + '/' +  version
 
 print("Creating " + my_path + " ...")
 if not os.path.exists(my_path):
